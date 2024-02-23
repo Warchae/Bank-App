8520f4a1b0e7b1690513d7a3d039758e5603a849
quantumZephyr <atakanemir.caldag@gmail.com>
2024-02-23 18:30:21 +0300

    Class Adaption to Old Bank.py

--- Bank App.py
+++ Bank App.py
@@ -1,50 +1,27 @@
-'''
-A Bank App Which is Just Register and Login Withdraw and Deposit Money To Your Account.
-We need Bank Account, Money Trace, Account Credits.
-'''
-#Creating userCredits List Because I Don't Know SQL
-userCredits = []
+from abc import ABC , abstractclassmethod
 
-# Register Function to the Bank with using in-Python list (which is the worst and not even logical decision).
-def registerfunc():
-    regNationalID = str(input("Enter Your National ID For REGISTER: "))
-    while len(regNationalID) != 11:
-        regNationalID = str(input("Please Enter Your 11 Digit National ID: "))
-        
-    regPassword = str(input("Enter Your 6 Digit Password For REGISTER: "))
-    while len(regPassword) < 5 or len(regPassword) >7:
-        regPassword = str(input("Please Enter Your 6 Digit Password: "))
+class Customer():
     
-    userCredits.append([regNationalID,regPassword])
-    return main()
+    def __init__(self,name,lastName,address,id,password):
+        self.name = name
+        self.lastName = lastName
+        self.address = address
+        self.id = id
+        self.password = password
 
-#Login Function To the BANK with registered ID and Password
-def loginfunc():
-    loginID = input("Please Enter Your ID For Login: ")
-    while len(loginID) != 11:
-        loginID = input("Your ID Must Be 11 Digit! Please Try Again: ")
+
+class BankAccount():
+    def __init__(self,balance,credit,accountID):
+        pass
+
+    def moneyTransfer():
+        pass
     
-    loginPass = input("Please Enter Your Password: ")
-    while len(loginPass) != 6:
-        loginPass = input("Your Password must be 6 Digit! Please Try Again: ")
-        
-    #Control For List In List Or Not
-    if [loginID,loginPass] in userCredits:
-        loggedIn = 1
-        print("Welcome to Our Bank.")
-    else:
-        print("Your Informations Doesn't Match Or You Are Not Registered Yet.\nDirecting To The Main Menu")
-        return main()
+    def moneyDeposit():
+        pass
 
-#Creating Main Menu For The Bank App Login Page
-def main():
-    command = input("1) Register\n2) Login\n3) Exit\nPlease Enter Your Command: ")
-#Change if commands to "switch-case" module
-    if command == "1":
-        registerfunc()
-    elif command == "2":
-        loginfunc()
-    else:
-        exit()
+    def moneyWithdraw():
+        pass
 
-main()
+    def takeCredit():
+        pass
